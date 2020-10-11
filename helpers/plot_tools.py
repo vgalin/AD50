c38bec4d98b47a3578cda921dd9fb182fc0d9a8e
vgalin <47885533+vgalin@users.noreply.github.com>
2020-10-11 22:28:06 +0200

    dirty commit + add logistic regression on pima indians dataset

---
+++ helpers/plot_tools.py
@@ -0,0 +1,75 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
+
+def plot_decision_regions_OG(X_set, y_set, title, x_label, y_label, c1=0, c2=1, classes=('red', 'blue'), model=None):
+    plt.figure()
+
+    X1, X2 = np.meshgrid(
+        np.arange(start=X_set[:, c1].min() - 1, stop=X_set[:, c1].max() + 1, step=0.01),
+        np.arange(start=X_set[:, c2].min() - 1, stop=X_set[:, c2].max() + 1, step=0.01),
+    )
+
+    if model:
+        plt.contourf(X1, X2, model.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), # ???
+                     alpha=0.25, cmap=ListedColormap(classes))
+
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(
+            X_set[y_set == j, 0],
+            X_set[y_set == j, 1],
+            # c = ListedColormap(classes)(i), # nop
+            color=ListedColormap(classes)(i),
+            label=j,
+        )
+
+    plt.title(f'Model ({title})')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend()
+    plt.show()
+    
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=cl, 
+                    edgecolor='black')
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100, 
+                    label='test set')
